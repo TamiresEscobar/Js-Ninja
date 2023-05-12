6413adaf0c910cd331d09c104ef96113bfde2f6b
Tamires-Escobar <tamires.escobar@corebiz.ag>
2023-05-11 21:11:07 -0300

    desafio-1

---
+++ Change#1.js
@@ -0,0 +1,52 @@
+
+// - declarar uma variavel 'myvar', sem valor
+var myVar 
+---------------------------------------------
+
+// - atribuir o valor 10 a variavel 'myvar'
+  myVar = 10
+--------------------------------------------
+
+// - declare uma nova variavel chama 'soma', e add uma instrucao somando os valores 15 e 8
+var soma = 15 + 8
+--------------------------------------------
+
+// - atribua a variavel 'soma' todo o valor dela, somando 1 usando operador de soma abreviado
+++soma
+--------------------------------------------
+
+// - atribua a variavel 'soma' todo o valor dela multiplicando por 3 usando o operador de multiplicação abreviado
+soma *= 3
+-------------------------------------------
+
+// - qual é o valor da variavel soma?
+72
+------------------------------------------
+
+// -declarar variavel chamada souninja, atribuindo a ela o valor booleano true
+var souNinja = true 
+----------------------------------------
+
+// - delcare variavel comida que recebe um array com os valores arroz, feijao e ovo
+var comida = ['arroz', 'feijao', 'ovo']
+------------------------------------------
+
+// -digite a instrução que imprime o valor de feijão que está na variavel comida
+comida[1]
+------------------------------------------
+
+// - digite o codido que verifica se a variavel soma é igual a variavel myvar (testando seu tipo)
+soma === myVar
+------------------------------------------
+
+// - digite o codigo que verifica se a variavel myvar é menor ou igual a variavel soma
+myVar <= soma 
+-----------------------------------------
+
+// - crie uma funcao chamada divisao que receba como parametro dois numeros e retorne o resultado da divisao entre elas
+function divisao(a, b) {
+    return a / b
+}
+
+// - invoque a funcao criada acima passando os parametros 10 e 2
+divisao(10, 2)
